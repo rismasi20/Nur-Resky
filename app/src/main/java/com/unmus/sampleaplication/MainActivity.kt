583f31673564e91df3e389ae5328c7a88bc802ae
Nur Resky <nurreskhy620@gmail.com>
2022-10-03 15:57:51 +0900

    up

--- app/src/main/java/com/unmus/sampleaplication/MainActivity.kt
+++ app/src/main/java/com/unmus/sampleaplication/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
